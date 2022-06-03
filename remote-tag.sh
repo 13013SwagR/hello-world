7834cff54dbfc47abcf9777253ca9bd805299b86
Vincent Dansereau <dansereauvincent@gmail.com>
2022-06-03 13:24:50 -0400

    feat(release): Add image release -- fix manifest push

--- remote-tag.sh
+++ remote-tag.sh
@@ -6,6 +6,4 @@ TAG_OLD=heelo-world-0d7d9f2-20220603t132745
 TAG_NEW=test
 CONTENT_TYPE="application/vnd.docker.distribution.manifest.v2+json"
 MANIFEST=$(curl -H "Accept: ${CONTENT_TYPE}" -H "Authorization: Bearer Z2hwX0p0ZlZ4VUNzUFdLZ2l4ZFV6NUo3d0ZDRmRGc3pNejFBY3BMNwo=" "$REGISTRY_NAME/v2/$REPOSITORY/manifests/$TAG_OLD")
-echo $MANIFEST > manifest.json
-
-curl -X PUT -H "Content-Type: ${CONTENT_TYPE}" -H "Authorization: Bearer Z2hwX0p0ZlZ4VUNzUFdLZ2l4ZFV6NUo3d0ZDRmRGc3pNejFBY3BMNwo=" -d "$MANIFEST" "$REGISTRY_NAME/v2/$REPOSITORY/manifests/$TAG_NEW"
+curl -f -X PUT -H "Content-Type: ${CONTENT_TYPE}" -H "Authorization: Bearer Z2hwX0p0ZlZ4VUNzUFdLZ2l4ZFV6NUo3d0ZDRmRGc3pNejFBY3BMNwo=" -d "$MANIFEST" "$REGISTRY_NAME/v2/$REPOSITORY/manifests/$TAG_NEW"
